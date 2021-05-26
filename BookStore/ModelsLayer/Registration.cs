73efe8bf7d3f0e9322376dfdbe55362a26e51a11
Neha Kotarwar <nehak6296@gmail.com>
2021-05-26 18:15:29 +0530

    [Neha]Added:User Registration model is added

---
+++ BookStore/ModelsLayer/Registration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsLayer
+{
+    public class Registration
+    {      
+            [Key]
+            public int UserId { get; set; }
+
+            [Required]
+            public string FirstName { get; set; }
+
+            [Required]
+            public string LastName { get; set; }
+
+            [Required]
+            [RegularExpression("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", ErrorMessage = "E-mail is not valid. Please Entet valid email")]
+            public string Email { get; set; }
+
+            [Required]
+            [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", ErrorMessage = "Invalid Password")]
+            public string Password { get; set; }
+
+        
+    }
+}
