8b4ba75d43a70b7ae9d9d5f97a2c8a2cce9a5d8d
Neha Kotarwar <nehak6296@gmail.com>
2021-05-26 19:15:38 +0530

    [Neha]Added:Cart amodel is added and implemented addToCart in cart repository

---
+++ BookStore/ManagerLayer/Managers/CartManager.cs
@@ -0,0 +1,25 @@
+﻿using ManagerLayer.Interfaces;
+using ModelsLayer;
+using RepositoryLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerLayer.Managers
+{
+    public class CartManager : ICartManager
+    {
+        private readonly ICartRepo cartRepo;
+        public CartManager(ICartRepo cartRepo)
+        {
+            this.cartRepo = cartRepo;
+        }
+        public Cart AddToCart(Cart cartModel)
+        {
+            return this.cartRepo.AddToCart(cartModel);
+        }
+
+    }
+}
---
+++ BookStore/RepositoryLayer/Repositories/CartRepo.cs
@@ -0,0 +1,54 @@
+﻿using ModelsLayer;
+using RepositoryLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Repositories
+{
+    public class CartRepo : ICartRepo
+    {
+        private SqlConnection connection;
+
+        //To Handle connection related activities    
+        private void Connection()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["UserDbConnection"].ToString();
+            connection = new SqlConnection(connectionString);
+
+        }
+        public Cart AddToCart(Cart cartModel)
+        {
+            try
+            {
+                Connection();
+                SqlCommand cmd = new SqlCommand("sp_AddToCart", connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@BookId", cartModel.BookId);
+                cmd.Parameters.AddWithValue("@UserId", cartModel.UserId);
+                cmd.Parameters.AddWithValue("@CartBookQuantity", cartModel.CartBookQuantity);
+                connection.Open();
+                int i = cmd.ExecuteNonQuery();
+                connection.Close();
+                if (i >= 1)
+                    return cartModel;
+                else
+                    return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+    }
+}
