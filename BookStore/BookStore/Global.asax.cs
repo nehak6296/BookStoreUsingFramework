06df4ea05b927dc2162b45ab3e268a8752bfe5fc
Neha Kotarwar <nehak6296@gmail.com>
2021-06-08 15:26:41 +0530

    [Neha]Added:implemented dependency injection using Unity

--- BookStore/BookStore/Global.asax.cs
+++ BookStore/BookStore/Global.asax.cs
@@ -13,7 +13,7 @@ namespace BookStore
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-           
+            UnityConfig.RegisterComponents();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
            RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
